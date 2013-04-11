be255a9f2d22f9c8ebe63ca6e416d46e80ae2e02
Stephen Heuzey <sheuzey@gmail.com>
2013-04-11 19:55:19 -0400

    Initial Commit

---
+++ VisionTrust/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VisionTrust
+//
+//  Created by Stephen Heuzey on 4/11/13.
+//  Copyright (c) 2013 Stephen Heuzey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VisionTrust/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VisionTrust
+//
+//  Created by Stephen Heuzey on 4/11/13.
+//  Copyright (c) 2013 Stephen Heuzey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
