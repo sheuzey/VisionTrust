92e335477daa55d36a7ce5dd1c768ca76b97f695
Stephen Heuzey <sheuzey@gmail.com>
2013-04-17 22:17:12 -0400

    Added toolbar with buttons in correct format

--- VisionTrust/AdvancedSearchViewController.m
+++ VisionTrust/AdvancedSearchViewController.m
@@ -13,7 +13,10 @@
 @end
 
 @implementation AdvancedSearchViewController
+
 #define PICKER_VIEW_TAG 100
+#define CANCEL_BUTTON_TAG 110
+#define DONE_BUTTON_TAG 120
 
 - (void)viewDidLoad
 {
@@ -22,7 +25,7 @@
     self.actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil];
     [self.actionSheet setActionSheetStyle:UIActionSheetStyleBlackOpaque];
     
-    self.pickerToolBar = [[UIToolbar alloc] init];
+    self.pickerToolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
     [self.pickerToolBar setBarStyle:UIBarStyleBlack];
     
     self.pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 40, 0, 0)];
@@ -30,13 +33,20 @@
     self.pickerView.dataSource = self;
     self.pickerView.delegate = self;
     self.pickerView.tag = PICKER_VIEW_TAG;
-    
-    self.pickerToolBar = [[UIToolbar alloc] init];
-    [self.pickerToolBar setBarStyle:UIBarStyleBlack];
 }
 
 - (IBAction)searchButtonPressed:(id)sender {
     [self.delegate exitAdvancedSearch];
+}
+
+- (void)cancelButtonPressed
+{
+    
+}
+
+- (void)doneButtonPressed
+{
+    
 }
 
 - (IBAction)countryButtonPressed:(id)sender {
@@ -49,8 +59,29 @@
     }
     [self.actionSheet addSubview:self.pickerView];
 
-    //Add items to toolbar..
+    //Cancel Button..
+    NSMutableArray *barItems = [[NSMutableArray alloc] init];
+    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonPressed:)];
+    cancelButton.tag = CANCEL_BUTTON_TAG;
     
+    //Title Label..
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 180, 30)];
+    [titleLabel setTextAlignment:NSTextAlignmentCenter];
+    [titleLabel setTextColor:[UIColor whiteColor]];
+    [titleLabel setFont:[UIFont boldSystemFontOfSize:16]];
+    [titleLabel setBackgroundColor:[UIColor clearColor]];
+    [titleLabel setText:@"Country"];
+    UIBarButtonItem *titleButton = [[UIBarButtonItem alloc] initWithCustomView:titleLabel];
+    
+    //Done Button..
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneButtonPressed:)];
+    doneButton.tag = DONE_BUTTON_TAG;
+    
+    //Add to array, then add to toolbar..
+    [barItems addObject:cancelButton];
+    [barItems addObject:titleButton];
+    [barItems addObject:doneButton];
+    [self.pickerToolBar setItems:barItems animated:YES];
     
     [self.actionSheet addSubview:self.pickerToolBar];
     [self.actionSheet showInView:self.view];
