ac44925d6e62229e054f5568e29ee5dc93b2aaf5
Stephen Heuzey <sheuzey@gmail.com>
2013-04-25 21:57:29 -0400

    Coded spiritual update controller

---
+++ VisionTrust/UpdateSpiritualViewController.h
@@ -0,0 +1,13 @@
+//
+//  UpdateSpiritualViewController.h
+//  VisionTrust
+//
+//  Created by Stephen Heuzey on 4/25/13.
+//  Copyright (c) 2013 Stephen Heuzey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpdateSpiritualViewController : UITableViewController
+
+@end
