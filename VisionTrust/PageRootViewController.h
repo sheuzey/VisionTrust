e82102ac55a2ad1b0f6114cadaa986c5b4401928
Stephen Heuzey <sheuzey@gmail.com>
2013-04-29 18:00:43 -0400

    Implemented horizontal scrollview for child profile

---
+++ VisionTrust/PageRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageRootViewController.h
+//  VisionTrust
+//
+//  Created by Stephen Heuzey on 4/29/13.
+//  Copyright (c) 2013 Stephen Heuzey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageRootViewController : UIViewController
+
+@end
---
+++ VisionTrust/PageRootViewController.m
@@ -0,0 +1,38 @@
+//
+//  PageRootViewController.m
+//  VisionTrust
+//
+//  Created by Stephen Heuzey on 4/29/13.
+//  Copyright (c) 2013 Stephen Heuzey. All rights reserved.
+//
+
+#import "PageRootViewController.h"
+
+@interface PageRootViewController ()
+
+@end
+
+@implementation PageRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
